295cd52d9f0a288f66e93ac75bddac5bc040a575
LahiruHarshana <lharshana2002@gmail.com>
2023-11-02 22:01:16 +0530

    content updated

--- Main.py
+++ Main.py
@@ -120,31 +120,32 @@ def game_over():
                        font=('consolas',70), text="GAME OVER", fill="red", tag="gameover")
 
 window = Tk()
-window.title("Snake Game")
-window.resizable(False,False)
+window.title("Snake game")
+window.resizable(False, False)
 
 score = 0
-direction ='down'
+direction = 'down'
 
-lable = Label(window, text="score:{}".format(score),font= ('consolas',40))
-lable.pack()
+label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
+label.pack()
 
-canvas = Canvas(window, bg=BACKGROUND_COLOR,height=GAME_HEIGHT,width=GAME_WIDTH)
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
 
 window.update()
-window_width= window.winfo_width()
-window_height =window.winfo_height()
-screen_width =window.winfo_screenwidth()
-screen_height =window.winfo_screenheight()
 
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
 
-x = int((screen_width/2) -(window_width/2))
-y =int((screen_height/2) - (window_height/2))
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
 
-window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 snake = Snake()
 food = Food()
+
+next_turn(snake, food)
 
 window.mainloop()
