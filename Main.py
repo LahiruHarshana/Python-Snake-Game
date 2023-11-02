cd3190bc771d6e314579387c92a812654b1961c3
LahiruHarshana <lharshana2002@gmail.com>
2023-11-02 20:24:18 +0530

    content updated

--- Main.py
+++ Main.py
@@ -51,6 +51,6 @@ screen_height =window.winfo_screenheight()
 x = (screen_width/2) -(window_width/2)
 y =(screen_height/2) - (window_height/2)
 
-window.geometry(f"{window_width}x{window_height}")
+window.geometry(f"{window_width}x{window_height}+")
 
 window.mainloop()
