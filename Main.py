ff98e04ec197206b604a6cc95bf5a9e8b595cd41
LahiruHarshana <lharshana2002@gmail.com>
2023-11-02 20:37:59 +0530

    content updated

--- Main.py
+++ Main.py
@@ -23,7 +23,7 @@ class Food:
 
         self.coordinates = [x,y]
 
-        canvas.create_oval(x,y,x + SPACE_SIZE,y+SPACE_SIZE)
+        canvas.create_oval(x,y,x + SPACE_SIZE,y+SPACE_SIZE, fill=FOOD_COLOR)
 
 
 
